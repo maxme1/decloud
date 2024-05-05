d1b6abca6000f801ea35c7bea5070874ce24a812
Max <max@ira-labs.com>
2024-05-05 01:25:51 +0100

    cleanup

--- .svelte-kit/output/client/_app/immutable/entry/start.75bcf8e2.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.79f13f35.js";export{t as start};
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.afb8159d.js","_app/immutable/chunks/scheduler.3fa178e2.js","_app/immutable/chunks/index.e4b3e33d.js","_app/immutable/chunks/stores.710c1f89.js","_app/immutable/chunks/entry.79f13f35.js"];
-export const stylesheets = ["_app/immutable/assets/2.33055757.css"];
-export const fonts = [];
