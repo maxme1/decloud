aea83d8a398f6ac6fb11c7cfb36a515b8bbe2efb
Max <max@ira-labs.com>
2024-05-05 01:08:56 +0100

    cleanup

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.d61fd986.js","_app/immutable/chunks/scheduler.3fa178e2.js","_app/immutable/chunks/index.e4b3e33d.js","_app/immutable/chunks/stores.d56c4fdd.js","_app/immutable/chunks/entry.709cfef6.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.f81f045a.js","_app/immutable/chunks/scheduler.3fa178e2.js","_app/immutable/chunks/index.e4b3e33d.js","_app/immutable/chunks/stores.d56c4fdd.js","_app/immutable/chunks/entry.709cfef6.js"];
-export const stylesheets = ["_app/immutable/assets/2.33055757.css"];
-export const fonts = [];
